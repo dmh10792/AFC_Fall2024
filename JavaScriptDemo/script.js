c88df3cfbc541c4691c9ba715d1cd62abd39ee59
Desmond Herring <desmond.m.herring.mil@army.mil>
2024-08-27 12:03:21 -0500

    updating demo script

--- JavaScriptDemo/script.js
+++ JavaScriptDemo/script.js
@@ -1,1 +1,21 @@
-console.log("I made it!")
+//console.log("I made it!")
+let animal1 = "dog";
+let animal2 = "cat";
+let animal3 = `${animal1} ${animal2}`;//String autoformatting
+
+//console.log(animal3);
+
+function myFunc(one, two, condition) {
+    if(condition) {
+        console.log(`${one} ${two}`);
+    } else {
+        console.log(`${two} ${one}`);
+    }
+}
+
+//myFunc("one", "two", true);
+//myFunc("one", "two", false);
+
+for(let count = 1; count <= 5; count++) {
+    console.log(count);
+}
