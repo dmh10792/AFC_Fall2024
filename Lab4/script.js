2692ad6cd51e46106704f06540bf12d08c004982
Herring, Desmond M CW2 USARMY 10 MTN DIV 1 BCT (USA) <desmond.m.herring.mil@army.mil>
2024-09-18 14:00:51 -0500

    Updated

---
+++ Lab4/script.js
@@ -0,0 +1,43 @@
+
+let menuItems = [
+    {
+        name: "CheeseBurger",
+        description: "Your basic cheeseBurger with options for toppings and extra patties.",
+        price: 7.99,
+        imgUrl: "assets/menuImages/cheeseburger.webp"
+    },
+    {
+        name: "Fries",
+        description: "Our homemade seasoned fries.",
+        price: 2.99,
+        imgUrl: "assets/menuImages/fries.webp"
+    },
+    {
+        name: "Milk Shake",
+        description: "Our handcrafted milkshakes wit multiple flavor options.",
+        price: 4.99,
+        imgUrl: "assets/menuImages/milkShake.webp"
+    },
+    {
+        name: "HotDog",
+        description: "An all beef hotdog with a bun.",
+        price: 3.99,
+        imgUrl: "assets/menuImages/hotDog.jpg"
+    },
+    {
+        name: "Fried Oreos",
+        description: "Deep fried oreos coated with powdered sugar.",
+        price: "3.99",
+        imgUrl: "assets/menuImages/oreos.avif"
+    }
+];
+
+//to load the values into the menu
+// let loadMenu = () => {
+//     //let card =  document.getElementById("menu-card");
+//     //console.log(card);
+    
+// }
+
+
+
